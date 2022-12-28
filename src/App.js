d8852e18a2d446b63585bbe2f2a5a98f4201f58d
JaanMürk <jaan.murk@avalanchelabs.com>
2022-12-28 22:22:39 +0200

    refactoring categories

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ const App = () => {
         {
             id: 1,
             title: 'Hats',
-        }
+        },
+        {
+            id: 2,
+            title: 'Jackets',
+        },
+        {
+            id: 3,
+            title: 'Sneakers',
+        },
+        {
+            id: 4,
+            title: 'Womens',
+        },
+        {
+            id: 5,
+            title: 'Mens',
+        },
+
     ]
 
     return (
