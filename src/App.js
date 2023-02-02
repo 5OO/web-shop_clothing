b44a3fc09506f507fd44803ad807744c6cb115f8
5OO <git.hub@myrk.eu>
2023-02-02 16:48:07 +0200

    created persisting navigation bar -> a  top level component. outlet shall render anything else inside.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import Home from "./routes/home/home-component";
-import {Route, Routes} from "react-router-dom";
+import {Outlet, Route, Routes} from "react-router-dom";
+
+const Navigation = () => {
+    return (
+        <div>
+            <div>
+                <h1>HI!  nav bar here! öäüõ ÜÕÖÄ</h1>
+            </div>
+            <Outlet/>
+        </div>
+    );
+};
+const Shop = () => {
+    return <h1>this is SHOP here öäüõ ÜÕÖÄöäüõ ÜÕÖÄöäüõ ÜÕÖÄ</h1>
+};
 
 const App = () => {
     return (
         <Routes>
-            <Route path='/' element={<Home/>}/>
+            <Route path='/' element={<Navigation />}>
+                <Route index element={<Home />}/>
+                <Route path='shop' element={<Shop />}/>
+            </Route>
         </Routes>
     );
 };
