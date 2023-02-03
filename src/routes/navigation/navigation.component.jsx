95ade13f6d46d5f1e1257cd51b3c0ea8f5b8372b
5OO <git.hub@myrk.eu>
2023-02-03 23:10:37 +0200

    added styles in index and to navigation bar in navigation.styles.scss and logo;

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,14 @@
 import {Link, Outlet} from "react-router-dom";
 import {Fragment} from "react";
+import {ReactComponent as CrownLogo} from '../../assets/crown.svg';
+import './navigation.styles.scss'
 
 const Navigation = () => {
     return (
         <Fragment>
             <div className='navigation'>
                 <Link className='logo-container' to='/'>
-                    <div>Logo</div>
+                    <CrownLogo classname='logo'/>
                 </Link>
                 <div className='nav-link-container'>
                     <Link className='nav-link' to='/shop'>
