e93d8de8028d1ece644796d9bfd4714cce141be8
5OO <git.hub@myrk.eu>
2023-02-03 16:45:10 +0200

    easter egg

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import Home from "./routes/home/home-component";
 import {Route, Routes} from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 
+// Easter egg
+console.log(
+   '\n' +
+    '   _____                 __    \n' +
+    '  /     \\ ___.__._______|  | __\n' +
+    ' /  \\ /  <   |  |\\_  __ \\  |/ /\n' +
+    '/    Y    \\___  | |  | \\/    < \n' +
+    '\\____|__  / ____| |__|  |__|_ \\\n' +
+    '        \\/\\/                 \\/\n');
+
 const Shop = () => {
     return <h1>this is SHOP here öäüõ ÜÕÖÄöäüõ ÜÕÖÄöäüõ ÜÕÖÄ</h1>
 };
@@ -10,6 +20,7 @@ const App = () => {
     return (
         <Routes>
             <Route path='/' element={<Navigation/>}>
+                debugger;
                 <Route index element={<Home/>}/>
                 <Route path='shop' element={<Shop/>}/>
             </Route>
